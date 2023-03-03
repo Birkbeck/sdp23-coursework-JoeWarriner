dac53b853c5c1166187caa4a73d494727a8efa87
Joe Warriner <joewarriner40@gmail.com>
2023-03-03 17:19:32 +0000

    Added test for getInstruction

---
+++ test/sml/TranslatorTest.java
@@ -0,0 +1,28 @@
+package sml;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import sml.instruction.AddInstruction;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Locale;
+
+import static sml.Registers.Register;
+
+public class TranslatorTest {
+
+    @Test
+    void getInstructionTest() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        Class<Translator> translatorClass = Translator.class;
+        Method getInstruction = translatorClass.getDeclaredMethod("getInstruction", String.class);
+        getInstruction.setAccessible(true);
+        Field lineField = translatorClass.getDeclaredField("line");
+        lineField.setAccessible(true);
+        Translator testTranslator = translatorClass.getDeclaredConstructor(String.class).newInstance("filename");
+        lineField.set(testTranslator, "add EAX EBX");
+        Object outputInstruction = getInstruction.invoke(testTranslator, "");
+        Assertions.assertTrue(outputInstruction instanceof AddInstruction);
+    }
+}
